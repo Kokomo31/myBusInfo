474c401b2b28571f84bcc22427dee1913b47b9dd
Kokomo31 <jdq6105@gmail.com>
2024-08-07 22:09:58 +0900

    first Project commit

---
+++ src/main/java/com/mybusinfo/mybusinfo/MybusinfoApplication.java
@@ -0,0 +1,13 @@
+package com.mybusinfo.mybusinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybusinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybusinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybusinfo/mybusinfo/MybusinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybusinfo.mybusinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybusinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
